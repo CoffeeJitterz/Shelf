61bc431f341bde275497814ad2c19cf318de7634
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-24 23:01:00 +0000

    saved stash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios';
 //import { render } from "react-dom";
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate, Switch } from 'react-router-dom';
 //import Bookmark from './components/Home/Bookmark';
 import Login from './components/Login/Register/Login';
 import Register from './components/Login/Register/Register';
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar/Navbar'
 function App() {
 
   useEffect(() => {
-    axios.get('http://localhost:3000/')
+    axios.get('http://localhost:3000/login')
     .then(res => {
       console.log(res);
     })}, [])
@@ -24,7 +24,7 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/home" element={<Home/>} />
-        <Route path="/login" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
         {/* <Route
         path="/login"
@@ -38,3 +38,114 @@ function App() {
 }
 
 export default App;
+// import React, { Component } from 'react';
+// //import axios from 'axios'
+// //import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// //import Home from './components/Home'
+// //import Login from './components/registrations/Login'
+// //import Signup from './components/registrations/Signup'
+
+// class App extends Component {
+
+//   // use the component’s state to maintain the logged in status of a User
+//   // and to store the User data when we request it from the server
+//   state = {
+//     isLoggedIn: false,
+//     user: {}
+//   }
+
+//   handleLogin = (data) => {
+//     this.setState({
+//       isLoggedIn: true,
+//       user: data.user
+//     })
+//   }
+
+//   handleLogout = () => {
+//     this.setState({
+//       isLoggedIn: false,
+//       user: {}
+//     })
+//   }
+
+//   loginStatus = () => {
+//     // GET API response data
+
+//     // {withCredentials: true}, allows our Rails server to set and read the cookie on the front-end’s browser
+//     // always pass this arg
+
+//     // Rails /logged_in, route and its corresponding controller action
+//     // App.js component communicates with Rails through this route 
+//     // If the User is verified in the Rails server, then a logged_in boolean is returned, along with the user object
+//     // App.js uses this response data to maintain the logged in status in the front-end
+
+//     axios.get('http://localhost:3001/login',
+//       { withCredentials: true })
+//       .then(response => {
+
+//         if (response.data.login) {
+//           this.handleLogin(response)
+//         } else {
+//           this.handleLogout()
+//         }
+
+//       })
+//       .catch(error => console.log('api errors:', error))
+//   }
+
+//   componentDidMount() {
+//     // keep track of status and request this information every time it’s mounted
+//     this.loginStatus()
+//   }
+
+//   // app won't render itself to DOM
+//   // will serve as our router to render all other components
+//   // will also manage the application’s state and authentication status
+
+//   render() {
+//     // rendering components using render=props
+//     // allows us to pass props to the components to be rendered
+//     // in this way, we can pass 
+//     // isLoggedIn state status, handleLogin(), and handleLogout(), 
+//     // to our components as props
+
+//     // can also pass the User object from state down to the necessary components, not passing User currently
+
+//     return (
+//       <div>
+
+//         <BrowserRouter>
+
+//           <Routes>
+
+//             <Route
+//               exact path='/'
+//               render={props => (
+//                 <Home {...props} loggedInStatus={this.state.isLoggedIn} />
+//               )}
+//             />
+
+//             <Route
+//               exact path='/login'
+//               render={props => (
+//                 <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
+//               )}
+//             />
+
+//             {/* <Route
+//               exact path='/signup'
+//               render={props => (
+//                 <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
+//               )}
+//             /> */}
+
+//           </Routes>
+
+//         </BrowserRouter>
+        
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- frontend/src/components/Login/Register/Login.js
+++ frontend/src/components/Login/Register/Login.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
-//import { browserHistory } from 'react-router';
-//import { useHistory } from "react-router-dom";
+import { browserHistory } from 'react-router';
+import { useHistory } from "react-router-dom";
 
 
 
@@ -87,5 +87,71 @@ export default function Login(props){
 // }
  
 // export default Login;
+// import React, { Component } from 'react';
+// import axios from 'axios'
+// import { Link } from 'react-router-dom'
 
+// class Login extends Component {
+
+//     state = {
+//         username: '',
+//         email: '',
+//         password: '',
+//         errors: ''
+//     }
+
+//     handleChange = (event) => {
+//         const { name, value } = event.target
+
+//         this.setState({
+//             [name]: value
+//         })
+
+//     };
+
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//     };
+
+//     render() {
+//         const { username, email, password } = this.state
+
+//         return (
+//             <div>
+//                 <h1>Log In</h1>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input
+//                         placeholder="username"
+//                         type="text"
+//                         name="username"
+//                         value={username}
+//                         onChange={this.handleChange}
+//                     />
+//                     <input
+//                         placeholder="email"
+//                         type="text"
+//                         name="email"
+//                         value={email}
+//                         onChange={this.handleChange}
+//                     />
+//                     <input
+//                         placeholder="password"
+//                         type="password"
+//                         name="password"
+//                         value={password}
+//                         onChange={this.handleChange}
+//                     />
+//                     <button placeholder="submit" type="submit">
+//                         Log In
+//                     </button>
+//                     <div>
+//                         or <Link to='/signup'>sign up</Link>
+//                     </div>
+
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+// export default Login;
 
