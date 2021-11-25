5f5aa444e3293d5ddf6843f3d1bd5dc71fc29c59
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-25 02:58:03 +0000

    add style to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import axios from 'axios';
-import { useState, useEffect} from 'react';
-import { BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom';
+//import axios from 'axios';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './components/Login/Register/Login';
 import Register from './components/Login/Register/Register';
 import Home from './components/Home/Home';
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles/navbar.css'
 
 export default function Navbar(props){
   return (
 
     <nav>
+      <h1>URLShelves</h1>
+
       <Link to="/home">Home</Link>
 
       <Link to="/login">Login</Link>
@@ -12,5 +14,6 @@ export default function Navbar(props){
       <Link to="/register">Register</Link>
     </nav>
 
+    
   )
 }
