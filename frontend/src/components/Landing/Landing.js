c603adfaf8f8ca2cc996cffb543e2a2a6908fc4a
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-30 18:35:11 +0000

    add landing page

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,1 +1,21 @@
 
+import App from "../../App"
+import books from '../books.jpeg'
+
+import Login from '../Login/Login'
+
+import './styles/landing.css'
+
+export default function Landing(props){
+const {loggedIn, setloggedIn} = props;
+
+  return (
+   <div className="landing_container"> 
+     <Login loggedIn={loggedIn} setloggedIn={setloggedIn}/>
+    <div>
+    <img src={books} />
+    </div>
+   </div>
+
+  )
+}
