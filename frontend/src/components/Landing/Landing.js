5761829a7780b4d8f4287533424c0e8a870caba4
Josh Millard <josh.millard10@gmail.com>
2021-11-30 18:02:45 -0500

    landing page image added

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,6 +1,9 @@
 
+import React, { useState } from "react";
 import App from "../../App"
 import bookmark from '../bookmark.jpeg'
+import books1 from '../books1.png'
+import books2 from '../books2.png'
 
 import Login from '../Login/Login'
 
@@ -8,13 +11,34 @@ import './styles/landing.css'
 
 export default function Landing(props){
 const {loggedIn, setloggedIn} = props;
+//Create modes for handelClick (toggle)
+const First = 'First';
+const Second = 'Second'
+  const [mode, setMode] = useState(Second)
+  const handleHover = () => {
+
+      if (mode === First) {
+        setMode(Second)
+        console.log(mode)
+      } else {
+        setMode(First)
+        console.log(mode)
+      }
+  };
 
   return (
    <div className="landing_container"> 
      <Login loggedIn={loggedIn} setloggedIn={setloggedIn}/>
-    
-    <img src={bookmark} />
-   
+    {mode === First && (
+      <div onMouseEnter={handleHover}>
+        <img src={books1} /> 
+      </div>
+    )}
+    {mode === Second && (
+       <div onMouseEnter={handleHover}>
+         <img src={books2} />       
+       </div>
+    )}
    </div>
 
   )
