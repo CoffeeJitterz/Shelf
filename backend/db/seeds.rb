32db4cc9beb7dc89114b2ebab7f4f83f8a1c3071
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-30 22:22:57 +0000

    update seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -25,7 +25,7 @@ jimmyShelf2 = Shelf.create(name: 'Games', user_id: user1.id)
 jimmyShelf3 = Shelf.create(name: 'News', user_id: user1.id)
 jimmyShelf4 = Shelf.create(name: 'Movies', user_id: user1.id)
 jimmyShelf5 = Shelf.create(name: 'Blogs', user_id: user1.id)
-jimmyShelf6 = Shelf.create(name: 'Articals', user_id: user1.id)
+jimmyShelf6 = Shelf.create(name: 'Articles', user_id: user1.id)
 
 
 category1 = Category.create(name: 'Entertainment')
@@ -43,7 +43,7 @@ bookmark5 = Bookmark.create(name: 'Hudson Bay', url: 'www.thebay.com', descripti
 #Jimmy's Bookmarks main shelf
 jimmyBookmark1 = Bookmark.create(name: 'Facebook', url: 'https://www.facebook.com/', description: 'Social media app', color: 'Blue', shelf_id: jimmyShelf1.id)
 jimmyBookmark2 = Bookmark.create(name: 'Google', url: 'https://www.google.com/', description: 'search engine', color: 'Orange', shelf_id: jimmyShelf1.id)
-jimmyBookmark3 = Bookmark.create(name: 'Lighthouse Labs', url: 'www.lighthouselabs.ca', description: 'Education', color: 'Green', shelf_id: jimmyShelf1.id)
+jimmyBookmark3 = Bookmark.create(name: 'Lighthouse Labs', url: 'https://www.lighthouselabs.ca', description: 'Education', color: 'Green', shelf_id: jimmyShelf1.id)
 jimmyBookmark4 = Bookmark.create(name: 'Youtube', url: 'https://www.youtube.com/', description: 'music app', color: 'Yellow', shelf_id: jimmyShelf1.id)
 jimmyBookmark5 = Bookmark.create(name: 'Hudson Bay', url: 'https://www.thebay.com/', description: 'shopping', color: 'Pink', shelf_id: jimmyShelf1.id)
 
@@ -52,22 +52,28 @@ jimmyBookmark6 = Bookmark.create(name: 'Mini Clip', url: 'https://www.miniclip.c
 jimmyBookmark7 = Bookmark.create(name: 'Addicting Games', url: 'https://www.addictinggames.com/', description: 'shopping', color: 'Pink', shelf_id: jimmyShelf2.id)
 jimmyBookmark8 = Bookmark.create(name: 'Coffee Break Arcade', url: 'https://www.coffeebreakarcade.com/', description: 'shopping', color: 'Pink', shelf_id: jimmyShelf2.id)
 
+
 #Jimmy's Bookmarks news shelf
 jimmyBookmark9 = Bookmark.create(name: 'CBC News', url: 'https://www.cbc.ca/news', description: 'shopping', color: 'Pink', shelf_id: jimmyShelf3.id)
 jimmyBookmark10 = Bookmark.create(name: 'Guelph News', url: 'https://www.guelphtoday.com/', description: 'shopping', color: 'Pink', shelf_id: jimmyShelf3.id)
+jimmyBookmark14 = Bookmark.create(name: 'Halton News', url: 'https://www.insidehalton.com/milton-on/', description: 'news', color: 'Blue', shelf_id: jimmyShelf3.id)
+jimmyBookmark15 = Bookmark.create(name: 'Milton Weather', url: 'https://www.theweathernetwork.com/ca/hourly-weather-forecast/ontario/milton', description: 'news', color: 'Yellow', shelf_id: jimmyShelf3.id)
+
 
 #Jimmy's Bookmarks Movies shelf
 jimmyBookmark11 = Bookmark.create(name: 'Netflix', url: 'https://www.netflix.com/ca/', description: 'movies', color: 'pink', shelf_id: jimmyShelf4.id)
 jimmyBookmark12 = Bookmark.create(name: 'Prime Videos', url: 'https://www.amazon.ca/b?ie=UTF8&node=16040885011', description: 'movies', color: 'pink', shelf_id: jimmyShelf4.id)
+
+
 
 #Jimmy's Bookmarks Blogs shelf
 jimmyBookmark11 = Bookmark.create(name: 'Freecodecamp', url: 'https://www.freecodecamp.org/news/tag/trees/', description: 'articles', color: 'pink', shelf_id: jimmyShelf5.id)
 jimmyBookmark11 = Bookmark.create(name: 'Medium', url: 'https://medium.com/', description: 'movies', color: 'pink', shelf_id: jimmyShelf5.id)
 
 #Jimmy's Bookmarks Articles shelf
-jimmyBookmark12 = Bookmark.create(name: 'wekipedia-Coding', url: 'https://en.wikipedia.org/wiki/Coding', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
-jimmyBookmark13 = Bookmark.create(name: 'wekipedia-React', url: 'https://en.wikipedia.org/wiki/React', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
-jimmyBookmark14 = Bookmark.create(name: 'wekipedia-Javascripts', url: 'https://en.wikipedia.org/wiki/JavaScript', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
+jimmyBookmark12 = Bookmark.create(name: 'wikipedia-Coding', url: 'https://en.wikipedia.org/wiki/Coding', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
+jimmyBookmark13 = Bookmark.create(name: 'wikipedia-React', url: 'https://en.wikipedia.org/wiki/React', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
+jimmyBookmark14 = Bookmark.create(name: 'wikipedia-Javascripts', url: 'https://en.wikipedia.org/wiki/JavaScript', description: 'articles', color: 'pink', shelf_id: jimmyShelf6.id) 
 
 bookmarks_categories1 = BookmarksCategory.create(bookmarks_id: bookmark1.id, categories_id: category1.id)
 bookmarks_categories2 = BookmarksCategory.create(bookmarks_id: bookmark2.id, categories_id: category2.id)
