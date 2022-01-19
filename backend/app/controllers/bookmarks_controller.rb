9759d98d0e1857530549d1b7e0c11e1664ddbb22
Gagandeep Uppal <guppal.profile@gmail.com>
2022-01-18 20:19:20 -0500

    cleaning code

--- backend/app/controllers/bookmarks_controller.rb
+++ backend/app/controllers/bookmarks_controller.rb
@@ -5,31 +5,9 @@ class BookmarksController < ApplicationController
     render json: @bookmarks
   end
 
-  # def create
-  #   @bookmark = Bookmark.create(params[:bookmark])
-  #   if @bookmark
-  #     flash[:success] = "Thanks for adding new bookmark"
-  #     redirect_to @bookmark
-  #   else
-  #     flash[:error] = 'Error, please try again.'
-  #   redirect_to @bookmark
-  #   end
 
    def create
-  #   @bookmark = Bookmark.create(bookmark_params)
-  #   if @bookmark
-  #     flash[:success] = "Thanks for adding new bookmark"
-  #     render json: @bookmark
-  #   else
-  #     flash[:error] = 'Error, please try again.'
-  #   redirect_to @bookmark
-  #   end
-  # end
-  # def bookmark_params 
-  #   params.require(:bookmark).permit(:name, :url, :color)
-  # end
-
-  bookmark = Bookmark.create(bookmark_params)
+    bookmark = Bookmark.create(bookmark_params)
     render json: bookmark
   end
   
